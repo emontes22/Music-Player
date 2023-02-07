91322d46d5c353e24b8869b085e973066d51fb1a
emontes22 <ervey025@gmail.com>
2023-02-07 14:43:40 -0700

    Work on forward and backward buttons

--- script.js
+++ script.js
@@ -1,26 +1,91 @@
-const music = document.querySelector('audio');
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "song1",
+    displayName: "A Small Miracle",
+    artist: "Romarecord1973",
+  },
+  {
+    name: "song2",
+    displayName: "Inspiring Emotional Uplifting Piano",
+    artist: "Music For Videos",
+  },
+  {
+    name: "song3",
+    displayName: "Piano Moment",
+    artist: "Daddys Music",
+  },
+  {
+    name: "song4",
+    displayName: "Relaxing Birds and piano music",
+    artist: "InnerTune",
+  },
+];
 
 // Check if playing
 let isPlaying = false;
 
 // Play
-function playSong(){
-	isPlaying = true;
-	playBtn.classList.replace('fa-play', 'fa-pause');
-	playBtn.setAttribute('title', 'Pause');
-	music.play()
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
 }
 
 // Pause
-function pauseSong(){
-	isPlaying = false;
-	playBtn.classList.replace('fa-pause', 'fa-play');
-	playBtn.setAttribute('title', 'Play');
-	music.pause()
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
 }
 
-// Play or Pause event 
-playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+// Play or Pause event
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `gifs/${song.name}.gif`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Select first song
+loadSong(songs[songIndex]);
+
+// Next Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
